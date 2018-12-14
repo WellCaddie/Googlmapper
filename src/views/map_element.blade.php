146494f0bc6ba0e64540c1ea26bd545c58b3f4ec
Bryson Reece <breece@wellcaddie.com>
2018-12-14 16:37:22 -0600

    Removed Map width/height ternary operator

--- src/views/map_element.blade.php
+++ src/views/map_element.blade.php
@@ -1,1 +1,1 @@
-<div id="map-canvas-{!! $id !!}" style="width: {{ $options['width'] ?: '100%' }}; height: {{ $options['height'] ?: '100%' }}; margin: 0; padding: 0; position: relative; overflow: hidden;"></div>
+<div id="map-canvas-{!! $id !!}" style="width: {{ $options['width'] }}; height: {{ $options['height'] }}; margin: 0; padding: 0; position: relative; overflow: hidden;"></div>
